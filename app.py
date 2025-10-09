68baa5ed95fd06cfa6db64c35a82923750b80807
Billy <xm9G@outlook.com>
2025-10-09 16:45:21 +1100

    fix map

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def mapPage(mode):
 
 @app.route('/api/locations/<mode>')
 def apiLocations(mode):
-    if not request.referrer or not request.referrer.startswith(request.host_url):
+    if not request.referrer or not request.referrer.startswith(request.host_url) or 'xm9g.net' in request.referrer:
         return jsonify({"error": "Access denied"}), 403
     return jsonify(getVehiclePositions(mode))
  
